00a55c85cb66d4d7382d9031623c59363468ea75
Aroon Chande <ar0ch@users.noreply.github.com>
2015-08-31 15:23:20 -0400

    $var1 + $var1 = broken script

--- extract_unaligned_coords.sh
+++ extract_unaligned_coords.sh
@@ -8,6 +8,6 @@ i=1
 j=2
 while [ $j -lt $length ]; do
         awk -v j=$j 'FNR == j {printf $1" "}' $1 >> $1.unaligned && awk -v i=$i 'FNR == i {print $2}' $1 >>$1.unaligned
-        let i=$[$i +i]
+        let i=$[$i +1]
         let j=$[$j+1]
 done
